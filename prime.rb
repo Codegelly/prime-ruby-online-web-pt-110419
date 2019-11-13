527e94d045007e7deba9f9930b24ad48bb9f053d
Angelica  Thompson <i.thompson89@outlook.com>
2019-11-13 01:39:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime? (number)
+  if number.multiply 2 
+  
+  else 
+ number.collect do |numbers|
+    
+  end 
+  
+  puts ("Be sure to account for negative numbers!")
+end 
+end 
