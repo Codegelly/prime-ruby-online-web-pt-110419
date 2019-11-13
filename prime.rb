70bfa56f45393d4728d49d84d697fcb347db1224
Angelica  Thompson <i.thompson89@outlook.com>
2019-11-13 02:14:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime? (number)
   if number < 2
   false 
   else 
-    (2..number ).to_a.all? do |n|
-    number % n == 0 
+    (2..number - 1).to_a.all? do |n|
+    number % n != 0 
     end 
   
   end 
